1fc07f5bd5992325ee36ede76d4c33a566169b28
bachngocng <nnbach97@gmail.com>
2022-01-28 01:33:42 +0700

    done

---
+++ src/com/codegym/NumberGenerator.java
@@ -0,0 +1,30 @@
+package com.codegym;
+
+public class NumberGenerator implements Runnable {
+    private String name;
+
+    public NumberGenerator() {
+        this.name = "";
+    }
+
+    public NumberGenerator(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public void run(){
+        int hashCode = this.hashCode();
+
+        for (int i = 0; i < 10 ; i++) {
+            String msg ="[" + this.name + " hashCode=" +hashCode+ "]";
+            msg += " i = " + i;
+            System.out.println(msg);
+            try {
+                Thread.sleep(500);
+            } catch (InterruptedException e){
+                e.printStackTrace();
+            }
+        }
+
+    }
+}
